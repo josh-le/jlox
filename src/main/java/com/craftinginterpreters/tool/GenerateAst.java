a682b38612bf81622cbefbcac0c9f2596a799870
Josh LeBlanc <joshcapitals07@gmail.com>
2025-01-12 19:16:12 -0500

    began writing the script that outputs the ast classes

---
+++ src/main/java/com/craftinginterpreters/tool/GenerateAst.java
@@ -0,0 +1,29 @@
+package com.craftinginterpreters.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.List;
+
+public class GenerateAst {
+    public static void main(String [] args) throws IOException {
+        // takes on argument: output dir
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+        }
+        String outputDir = args[0];
+        // generate the ast
+        defineAst(outputDir, "Expr", Arrays.asList(
+            "Binary     : Expr left, Token operator, Expr right",
+            "Grouping   : Expr expression",
+            "Literal    : Object value",
+            "Unary      : Token operator, Expr right"
+        ));
+    }
+
+    private static void defineAst(
+        // do later
+    )
+
+}
