92512a3ecc839234d86b091712e71b3b209fc35f
Josh LeBlanc <joshcapitals07@gmail.com>
2025-01-12 21:34:39 -0500

    little more work on generate ast

--- src/main/java/com/craftinginterpreters/tool/GenerateAst.java
+++ src/main/java/com/craftinginterpreters/tool/GenerateAst.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GenerateAst {
-    public static void main(String [] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         // takes on argument: output dir
         if (args.length != 1) {
             System.err.println("Usage: generate_ast <output directory>");
@@ -15,15 +15,25 @@ public class GenerateAst {
         String outputDir = args[0];
         // generate the ast
         defineAst(outputDir, "Expr", Arrays.asList(
-            "Binary     : Expr left, Token operator, Expr right",
-            "Grouping   : Expr expression",
-            "Literal    : Object value",
-            "Unary      : Token operator, Expr right"
-        ));
+                    "Binary     : Expr left, Token operator, Expr right",
+                    "Grouping   : Expr expression",
+                    "Literal    : Object value",
+                    "Unary      : Token operator, Expr right"
+                    ));
     }
 
-    private static void defineAst(
-        // do later
-    )
+    private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
+        String path = outputDir + "/" + baseName + ".java";
+        PrintWriter writer = new Printwriter(path, "UTF-8");
+
+        writer.println("package com.craftinginterpreters.lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class " + baseName + " {");
+
+        writer.println("}");
+        writer.close();
+    }
 
 }
