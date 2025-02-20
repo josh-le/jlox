3e41ca374e7b5a0a8df6add883cce551c876f91a
Josh LeBlanc <joshcapitals07@gmail.com>
2025-02-20 14:49:27 +0100

    began implementing binary operator evaluation

--- src/main/java/com/craftinginterpreters/lox/Interpreter.java
+++ src/main/java/com/craftinginterpreters/lox/Interpreter.java
@@ -11,7 +11,44 @@ class Interpreter implements Expr.Visitor<Object> {
         return evaluate(expr.expression);
     }
 
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        Object right = evaluate(expr.right);
+
+        switch(expr.operator.type) {
+            case MINUS:
+                return -(double)(right);
+            case BANG:
+                return !isTruthy(right);
+        }
+
+        return null;
+    }
+
+    private boolean isTruthy(Object object) {
+        if (object == null) return false;
+        if (object instanceof Boolean) return (boolean)object;
+        return true;
+    }
+
     private Object evaluate(Expr expr) {
         return expr.accept(this);
+    }
+
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        Object left = evaluate(expr.left);
+        Object right = evaluate(expr.right);
+
+        switch (expr.operator.type) {
+            case MINUS:
+                return (double)left - (double)right;
+            case SLASH:
+                return (double)left / (double)right;
+            case STAR:
+                return (double)left * (double)right;
+        }
+
+        return null;
     }
 }
